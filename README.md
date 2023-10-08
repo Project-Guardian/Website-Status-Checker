efd1f839b01a8331302c487bd7bd191b03e3ad6c
Project-Guardian <58954114+Project-Guardian@users.noreply.github.com>
2023-10-08 12:19:36 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,108 @@
+Website Status Checker
+
+This Python script checks the status of websites listed in a JSON file, records the results, and updates the JSON file with the status of each website.
+Prerequisites
+
+    Python 3.x
+    Required Python packages: requests
+
+Usage
+
+    Clone this repository or download the Python script to your local machine.
+
+    Ensure you have Python 3.x installed on your system.
+
+    Install the required Python packages using pip:
+
+pip install requests
+
+Prepare a JSON file containing a list of websites you want to check. The JSON file should have the following structure. An example is provided file is provided:
+
+json
+
+{
+    "https://example.com": {
+        "Reached": null,
+        "Last Check Time": null,
+        "Last Fail Time": null,
+        "Status Code": null,
+        "Status Message": null
+    },
+    "https://example2.com": {
+        "Reached": null,
+        "Last Check Time": null,
+        "Last Fail Time": null,
+        "Status Code": null,
+        "Status Message": null
+    },
+    // Add more websites as needed
+}
+
+    Replace "https://example.com" and "https://example2.com" with the URLs of the websites you want to check.
+    Leave the other fields ("Reached", "Last Check Time", "Last Fail Time", "Status Code", "Status Message") as null or provide an empty dict, they will be created automatically.
+
+Run the script using the following command:
+
+python website_status_checker.py your_json_file.json
+
+Replace your_json_file.json with the path to your JSON file containing the list of websites.
+
+Optionally, use the -v or --verbose flag to enable verbose output. This will display additional information about the status of each URL.
+
+    python website_status_checker.py -v your_json_file.json
+
+Output
+
+The script will perform the following actions:
+
+    Send HTTP GET requests to each website listed in the JSON file.
+    Record the status code and status message for each website.
+    Update the "Reached", "Last Check Time", "Last Fail Time", "Status Code", and "Status Message" fields in the JSON file with the results of the checks.
+    "Status Code" is not update if the site is completely unreachable.
+
+If a website's status code falls within the range of 4xx or 5xx, it is considered unsuccessful, and the "Reached" field is set to False. If the website's status code is not in that range, it is considered successful, and the "Reached" field is set to True. The script also records the time of the last check and, if applicable, the time of the last failure.
+Example
+
+Here's an example of a JSON file before and after running the script:
+
+Before:
+
+json
+
+{
+    "https://example.com": {
+        "Reached": null,
+        "Last Check Time": null,
+        "Last Fail Time": null,
+        "Status Code": null,
+        "Status Message": null
+    },
+    "https://example2.com": {
+        "Reached": null,
+        "Last Check Time": null,
+        "Last Fail Time": null,
+        "Status Code": null,
+        "Status Message": null
+    }
+}
+
+After:
+
+json
+
+{
+    "https://example.com": {
+        "Reached": true,
+        "Last Check Time": "2023-10-10 14:30:00",
+        "Last Fail Time": null,
+        "Status Code": 200,
+        "Status Message": "Success"
+    },
+    "https://example2.com": {
+        "Reached": false,
+        "Last Check Time": "2023-10-10 14:35:00",
+        "Last Fail Time": "2023-10-10 14:35:00",
+        "Status Code": 404,
+        "Status Message": "Not Found"
+    }
+}
---
+++ website_status_checker.py
@@ -0,0 +1,113 @@
+import json
+import requests
+from datetime import datetime
+from urllib.parse import urlparse
+import argparse
+
+# Function to check the URL and return the status code and message
+def check_url(url, verbose):
+    try:
+        # Add a header
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0"
+        }
+
+        # Attempt to make an HTTP GET request to the provided URL
+        response = requests.get(url, headers=headers)
+        status_code = response.status_code
+        response.raise_for_status()  # Raise an exception for HTTP errors (4xx, 5xx)
+        if verbose:
+            # If verbose mode is enabled, print a success message
+            print(f"Status Message for {url}: Success \n")
+
+        return status_code, "Success"
+    except requests.exceptions.RequestException as e:
+        if verbose:
+            # If verbose mode is enabled, print an error message
+            print(f"Status Message for {url}: Failure")
+            print(f"{str(e)} \n")
+        
+        # Return the status code even on failure (4XX or 5XX)
+        return response.status_code if 'response' in locals() else None, str(e)
+
+# Function to update the JSON data with the results of the URL check
+def update_json(json_data, original_url, status_code, status_message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Create a new entry in the JSON data if the URL is not already present
+    if original_url not in json_data:
+        json_data[original_url] = {
+            "Reached": None,               # Initialize 'Reached' to None
+            "Last Check Time": None,       # Initialize 'Last Check Time' to None
+            "Last Fail Time": None,        # Initialize 'Last Fail Time' to None
+            "Status Code": None,           # Initialize 'Status Code' to None
+            "Status Message": None         # Initialize 'Status Message' to None
+        }
+
+    entry = json_data[original_url]  # Retrieve the entry for the original URL
+
+    if status_code is not None:
+        # If the URL check was successful:
+        entry["Last Check Time"] = timestamp  # Update the "Last Check Time" to the current timestamp
+        entry["Status Code"] = status_code   # Update the "Status Code" with the received status code
+        entry["Status Message"] = status_message  # Update the "Status Message" with the success message
+
+        # Check if the status code indicates an error (4XX or 5XX)
+        if 400 <= status_code < 600:
+            if entry["Reached"]:
+                # If the previous status was "Reached" (i.e., it was successful before):
+                entry["Last Fail Time"] = timestamp  # Update the "Last Fail Time" to the current timestamp
+                print(f"Status Message for {original_url}: Failure")
+                print(f"{status_message}")
+                print("Current failed time " + timestamp + "\n")
+            entry["Reached"] = False
+        else:
+            entry["Reached"] = True
+    else:
+        # If the URL check failed:
+        if entry["Reached"]:
+            # If the previous status was "Reached" (i.e., it was successful before):
+            entry["Last Fail Time"] = timestamp  # Update the "Last Fail Time" to the current timestamp
+            print(f"Status Message for {original_url}: Failure")
+            print(f"{status_message}")
+            print("Current failed time " + timestamp + "\n")
+
+        entry["Reached"] = False            # URL check failed
+        entry["Last Check Time"] = timestamp  # Update the "Last Check Time" to the current timestamp
+        entry["Status Message"] = status_message  # Update the "Status Message" with the failure message
+
+# Main function to perform the URL checks and update the JSON data
+def main(json_file, verbose):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+
+    # Create a copy of the keys to iterate over
+    urls = list(data.keys())
+
+    for original_url in urls:
+        # Check if the URL has a scheme (http:// or https://)
+        parsed_url = urlparse(original_url)
+        if not parsed_url.scheme:
+            temp_url = "https://" + original_url  # Add "https://" if scheme is missing
+        else:
+            temp_url = original_url  # Use the original URL for the request
+
+        if verbose:
+            # If verbose mode is enabled, print the URL being checked
+            print(f"Checking URL: {temp_url}")
+        
+        status_code, status_message = check_url(temp_url, verbose)
+        update_json(data, original_url, status_code, status_message)
+
+    with open(json_file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+if __name__ == "__main__":
+    # Set up command-line argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "-V", "--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("json_file", help="Path to the JSON file")
+    args = parser.parse_args()
+    
+    # Call the main function with parsed arguments
+    main(args.json_file, args.verbose)
